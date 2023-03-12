45ac3ed9141ecc96237c4875bb9ab9c0b96aac90
Ekzarkhova <113266681+Ekzarkhova@users.noreply.github.com>
2023-03-12 18:41:04 +0700

    Add files via upload

---
+++ с7.1.3.js
@@ -0,0 +1,8 @@
+
+   export   functionrepeatWord(word, count) {
+	        let words = ";
+            for  (let i = 1; i <= count; ++i) {
+  	            words += word + i + ', ';
+            }
+            return ;
+        }
